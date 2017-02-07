201cc33d94f4af3bbdb8e5552924f5dd350284ff
Don <dwan0505@hotmail.com>
2017-02-07 15:03:46 +0100

    sqlite

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
  
-  require 'rake'
-  Rake::Task.clear
-  Showcase::Application.load_tasks
+  #require 'rake'
+  #Rake::Task.clear
+  #Showcase::Application.load_tasks
   
   class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
@@ -10,9 +10,11 @@
   
   private
   def log_visitor
-	Rake::Task['db:migrate'].invoke
+	#Rake::Task['db:migrate'].invoke
 	@ip = Socket.ip_address_list
-	@ip2 =  request.env["HTTP_X_FORWARDED_FOR"]
+	@ip2 = request.remote_ip
+	@ip3 =  request.env["HTTP_X_FORWARDED_FOR"]
+	@ip4 = request.env['REMOTE_ADDR']
 	@local_ip = @ip[1].inspect.gsub("#<Addrinfo: ", "").gsub(">", "")
 	Login.create(ip: @local_ip, visited_at: Time.now)
   end
